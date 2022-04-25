f35a8106d73becc9248eeb9988bb9a1fa526aa74
majdsafira <majdsafira@gmail.com>
2022-04-25 16:23:14 +0300

    upload project

---
+++ php-25-4-2022/task1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php  
+$year = 2031;  
+ 
+if((0 == $year % 4) )
+{
+echo "$year is a Leap Year.";    
+}
+ 
+else  
+{  
+echo "$year is not a Leap Year.";    
+}
+?> 
+ 
+</body>
+</html>
